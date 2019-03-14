ad4ec83f6a21ca372f370a4e5b85a378da778c6a
Kim McClymont <Kim McClymont>
2019-03-14 15:08:04 +1100

    Merged PR 6245: Merge Initial-draft to master

--- projects/df-sharepoint-lib/src/public_api.ts
+++ projects/df-sharepoint-lib/src/public_api.ts
@@ -7,4 +7,5 @@ export * from  './lib/edit-document.service'
 export * from  './lib/sharepoint-rest.service'
 export * from  './lib/sharepoint-jsom.service'
 export * from  './lib/sharepoint-list.service'
+export * from  './lib/sp-utils'
 
